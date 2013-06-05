187dac99d6281cf1b0f70a38322a91e0d536d4d4
Matt Shirley <mdshw5@gmail.com>
2013-06-05 11:02:30 -0400

    cleanup directories

--- cgi-bin/triPOD/config.py
+++
@@ -1,9 +0,0 @@
-# Change these options to match system values
-htmlRoot = '/home/httpd/html'
-cgiRoot = '/home/httpd/cgi-bin'
-apacheRoot = '/home/httpd/html/triPOD/results/'
-apacheURL = 'http://pevsnerlab.kennedykrieger.org/'
-perlPath = '/usr/bin/perl'
-pythonPath = '/usr/bin/python26'
-authURL = 'http://review:testing@pevsnerlab.kennedykrieger.org/'
-cores = 2
